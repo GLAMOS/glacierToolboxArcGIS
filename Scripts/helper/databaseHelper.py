d83d709944b0cf207ce49d5d8eae4b0dd22b1f64
yweidmann <yweidmann@f515a3c9-8029-47f5-aa1f-c42dc02baa06>
2013-05-21 13:31:51 +0000

    - Implementation of the method getWorkspaceType(self, featureLayer) to retrieve the database type of the layer. Has to be used to set the appropriate SQL statement.

--- Scripts/helper/databaseHelper.py
+++ Scripts/helper/databaseHelper.py
@@ -2,6 +2,11 @@ import arcpy, uuid, ConfigParser, datetime, getpass, os
 
 DATABASE_MAPPING_FILE = "GlacierDatabaseMapping.cfg"
 
+DATABASE_TYPE_PERSONALGEODATABASE = "mdb"
+DATABASE_TYPE_FILEGEODATABASE = "gdb"
+DATABASE_TYPE_SPATIALDATABASEENGINE = "sde"
+DATABASE_TYPE_FILEBASED = "shp"
+
 class DatabaseHelper(object):
 
     def __init__(self):
@@ -27,3 +32,25 @@ class DatabaseHelper(object):
 
     def getDatabaseMapping(self, group, key):
         return self.__databaseMapping.get(group, key)
+
+    def getWorkspaceType(self, featureLayer):
+        
+        workspacePath = featureLayer.workspacePath
+        desWorkspacePath = arcpy.Describe(workspacePath)
+        workspaceType = desWorkspacePath.workspaceFactoryProgID
+
+        #Identifies File Geodatabase
+        if workspaceType == "esriDataSourcesGDB.FileGDBWorkspaceFactory.1":
+            return DATABASE_TYPE_FILEGEODATABASE
+
+        #Identifies Personal Geodatabase
+        elif workspaceType == "esriDataSourcesGDB.AccessWorkspaceFactory.1":
+            return DATABASE_TYPE_PERSONALGEODATABASE
+
+        #Identifies SDE database
+        elif workspaceType == "esriDataSourcesGDB.SdeWorkspaceFactory.1":
+            return DATABASE_TYPE_SPATIALDATABASEENGINE
+
+        #Other (Shapefile, coverage, CAD, VPF, and so on)
+        else:
+            return DATABASE_TYPE_FILEBASED
