d22c70fb45b3fbbf8ee3954db658e00f9542db49
yweidmann <yweidmann@f515a3c9-8029-47f5-aa1f-c42dc02baa06>
2013-09-13 12:55:47 +0000

    - Dealing only with valid geometries. Points with Non-geometries are ignored. - Correct indenting of code.

--- VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportGlacierData/exportPolyline2xyzn.py
+++ VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportGlacierData/exportPolyline2xyzn.py
@@ -29,19 +29,21 @@ def analyzeLines(sourceLayer):
     searchRows = arcpy.SearchCursor(sourceLayer)
 
     for searchRow in searchRows:
-       featureNumber = featureNumber + 1
-       fcVertexList.append([])
-       shapeObj = searchRow.getValue(shapeFieldName)
+        featureNumber = featureNumber + 1
+        fcVertexList.append([])
+        shapeObj = searchRow.getValue(shapeFieldName)
        
-       # Loop for SinglePart
-       #partObj = shapeObj.getPart(0)
-       #for pointObj in partObj:
-       #    fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
+        # Loop for SinglePart
+        #partObj = shapeObj.getPart(0)
+        #for pointObj in partObj:
+        #    fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
        
-       # Loop for MultiPart
-       for partObj in shapeObj:
-          for pointObj in partObj:
-             fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
+        # Loop for MultiPart
+        for partObj in shapeObj:
+            for pointObj in partObj:
+                # Handling only valid geometries.
+                if (pointObj != None):
+                    fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
 
 
     return fcVertexList
