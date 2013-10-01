34841058fe2554c572fc5d8aaf21d208b6b38a58
yweidmann <yweidmann@f515a3c9-8029-47f5-aa1f-c42dc02baa06>
2013-10-01 08:36:29 +0000

    - Single points exported as well with code = 11.

--- VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportGlacierData/exportPolyline2xyzn.py
+++ VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportGlacierData/exportPolyline2xyzn.py
@@ -3,6 +3,7 @@ import arcpy
 CODE_LINE_START = 21
 CODE_LINE_VERTEX = 22
 CODE_LINE_END = 23
+CODE_POINT = 11
 
 def doExport(sourceLayer, targetFile):
     arcpy.AddMessage("Start to export a line layer to a xyzn ASCII file ...")
@@ -32,18 +33,32 @@ def analyzeLines(sourceLayer):
         shapeObj = searchRow.getValue(shapeFieldName)
         
         arcpy.AddMessage("A new geometry will be analyzed.")
-       
-        # Loop for MultiPart
-        for partObj in shapeObj:
+        
+        if shapeObj.type == "point":
+            arcpy.AddMessage("A new point geometry was found.")
             
-            arcpy.AddMessage("A new part within the current geometry was found.")
             featureNumber = featureNumber + 1
             fcVertexList.append([])
             
-            for pointObj in partObj:
-                # Handling only valid geometries.
-                if (pointObj != None):
-                    fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
+            fcVertexList[featureNumber - 1].append([shapeObj.getPart().X, shapeObj.getPart().Y, shapeObj.getPart().Z])
+            
+            
+            
+        elif (shapeObj.type == "polygon" or shapeObj.type == "polyline"):
+            # Loop for MultiPart
+            for partObj in shapeObj:
+                
+                arcpy.AddMessage("A new part within the current geometry was found.")
+                featureNumber = featureNumber + 1
+                fcVertexList.append([])
+                
+                for pointObj in partObj:
+                    # Handling only valid geometries.
+                    if (pointObj != None):
+                        fcVertexList[featureNumber - 1].append([pointObj.X, pointObj.Y, pointObj.Z])
+        
+        else:
+            arcpy.AddMessage("The current type of geometry is not handled.")
 
 
     return fcVertexList
@@ -58,16 +73,22 @@ def exportAnalyzedLines(analyzedLines, targetFile):
 
             numberVertex = len(analyzedLine)
 
-            arcpy.AddMessage("Line found with " + str(numberVertex) + " vertex found.")
+            arcpy.AddMessage("Geometry with " + str(numberVertex) + " vertex found.")
 
             for i in range(0, numberVertex):
 
-                codeVertex = CODE_LINE_VERTEX
-
-                if i == 0:
-                    codeVertex = CODE_LINE_START
-                elif i == numberVertex - 1:
-                    codeVertex = CODE_LINE_END
+                if numberVertex == 1:
+                    
+                    codeVertex = CODE_POINT
+                
+                else:
+                    
+                    codeVertex = CODE_LINE_VERTEX
+    
+                    if i == 0:
+                        codeVertex = CODE_LINE_START
+                    elif i == numberVertex - 1:
+                        codeVertex = CODE_LINE_END
 
                 lineToWrite = str(analyzedLine[i][0]) + " " + str(analyzedLine[i][1]) + " " + str(analyzedLine[i][2]) + " " + str(codeVertex)
                 
