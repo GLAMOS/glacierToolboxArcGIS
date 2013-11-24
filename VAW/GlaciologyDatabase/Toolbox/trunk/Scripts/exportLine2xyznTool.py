accde1ca2898a02e7a38a58d4a20aa558fd7e60d
yweidmann <yweidmann@f515a3c9-8029-47f5-aa1f-c42dc02baa06>
2013-11-24 15:14:50 +0000

    - A field of the chosen dataset can be used to act as source for height information instead of the height of the individual vertex. Can be very useful in the case of exporting contour lines.

--- VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportLine2xyznTool.py
+++ VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportLine2xyznTool.py
@@ -2,5 +2,6 @@ import arcpy, exportGlacierData.exportPolyline2xyzn
 
 sourceLayer = arcpy.GetParameter(0)
 targetFile = arcpy.GetParameterAsText(1)
+altitudeField = arcpy.GetParameterAsText(2)
 
-exportGlacierData.exportPolyline2xyzn.doExport(sourceLayer, targetFile)
+exportGlacierData.exportPolyline2xyzn.doExport(sourceLayer, targetFile, altitudeField)
