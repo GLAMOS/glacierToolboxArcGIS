7607bb8290930973304d13ac079c91257651271f
yweidmann <yweidmann@f515a3c9-8029-47f5-aa1f-c42dc02baa06>
2014-08-20 11:45:05 +0000

    - Adding additional parameter for a point-ID field in case of point data with an ID

--- VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportLine2xyznTool.py
+++ VAW/GlaciologyDatabase/Toolbox/trunk/Scripts/exportLine2xyznTool.py
@@ -3,5 +3,6 @@ import arcpy, exportGlacierData.exportPolyline2xyzn
 sourceLayer = arcpy.GetParameter(0)
 targetFile = arcpy.GetParameterAsText(1)
 altitudeField = arcpy.GetParameterAsText(2)
+pointIdField = arcpy.GetParameterAsText(3)
 
-exportGlacierData.exportPolyline2xyzn.doExport(sourceLayer, targetFile, altitudeField)
+exportGlacierData.exportPolyline2xyzn.doExport(sourceLayer, targetFile, altitudeField, pointIdField)
